cdf0dbe0b27d352008f1c512bd7512ac2533fa56
NikolasSerg <nikolas.ser.a@yandex.ru>
2020-01-14 14:09:15 +0200

    first

---
+++ notes.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+
+const operation = process.argv[2];
+const file = process.argv[3];
+const post = process.argv[4];
+const description = process.argv[5];
+
+// console.log(operation === "list");
+
+switch (operation) {
+    // case create:
+    //     console.log("operation create");
+    //     break;
+    case 'list':
+        showList(file);
+        // console.log("list method done");
+        break;
+    case 'add':
+        console.log("add post");
+        addPost()
+        break;
+   
+
+    default:
+        console.log("don't know this command");
+        break;
+}
+function existFile(fileName) {
+    if (fs.existsSync(fileName)) {
+        console.log(`yeah, allright. file exist. go on`);
+    } else {
+        fs.writeFile(fileName,"", (err) => {
+            if (err) {
+                console.log(`sorry but somthing wrong: ${err}`);
+            } else {
+                console.log('its good for you:  file create');
+            }
+        })
+    }
+}
+ function readFile(file) {
+     let readableFile;
+    fs.readFile(file, (err, data) => {
+        if (err) {
+            if(err.code === 'ENOENT') {
+                console.log('file dont exist');
+            } else {
+                console.log(`somthing wrong: ${err}`);
+            }    
+        } else {
+            if(data.toString() == '') {
+                console.log(`empty ${file} file`);
+            } else {
+                let rez = JSON.parse(data);
+                console.log(rez);
+                console.log(rez['sdsd']);
+                readableFile = rez;
+                return readableFile;
+            }
+        }
+    }) 
+    let rez = fs.readFileSync(file);
+    console.log(rez.toString());
+    existFile(file);
+    fs.readFile(file, (err, data) => {
+        if (err) {
+            if(err.code === 'ENOENT') {
+                console.log('file dont exist');
+            } else {
+                console.log(`something wrong: ${err}`);
+            }    
+        } else {
+            if(data.toString() == '') {
+                console.log(`empty ${file} file`);
+            } else {
+                let rez = JSON.parse(data);
+                // console.log(rez);
+                for (const key in rez) {
+                    if (rez.hasOwnProperty(key)) {
+                        console.log(`${key}: ${rez[key]}`);
+                    }
+                }
+            }
+        }
+    })
+    // let rez = readFile(file);
+    // console.log(rez, 'rez');
+    
+}
+ function addPost(file, post, desription) {
+    existFile(file);
+
+    
+ }
